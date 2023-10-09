93db3d0c1d135e90b6e04de779e740ab22fb61a0
Dhruv Mohindru <dmohindru@gmail.com>
2023-10-10 07:10:45 +1100

    Boot strapped the counter app

---
+++ counter-app/react-counter-app/src/components/Counter.tsx
@@ -0,0 +1,11 @@
+export const Counter: React.FC = () => {
+    return (
+        <div>
+            <h3>Counter App</h3>
+            <button>Increment</button>
+            <button>Decrement</button>
+            <button>Reset</button>
+        </div>
+    )
+
+}
