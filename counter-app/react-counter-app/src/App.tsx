83bb98628d0a1a28a2c0a768ae34a23c2c859842
Dhruv Mohindru <dmohindru@gmail.com>
2023-10-11 21:59:21 +1100

    Few more UI improvements

--- counter-app/react-counter-app/src/App.tsx
+++ counter-app/react-counter-app/src/App.tsx
@@ -1,5 +1,3 @@
-
-
 import { Stack } from "@mui/material";
 import { AppHeader } from "./components/AppHeader";
 import { AlphaNumericCounter } from "./components/alpha-numeric-counter/AlphaNumericCounter";
@@ -10,7 +8,7 @@ const App: React.FC = () => {
   return (
       <>      
         <AppHeader />
-          <Stack direction='row'>
+          <Stack direction='row' spacing={4} sx={{flexGrow: 1, alignContent: 'center'}}>
             <AlphaNumericCounter />
             <NumericCounter />
           </Stack>
