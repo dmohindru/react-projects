397c8a468ebafa9573a5b9a6130d045c82ae11f8
Dhruv Mohindru <dmohindru@gmail.com>
2023-11-06 22:02:15 +1100

    Created Sidebar component

--- mui-dash-board-app/dash-board-frontend/src/App.tsx
+++ mui-dash-board-app/dash-board-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/TopBar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
+import { Route, Routes } from "react-router-dom";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
@@ -12,6 +13,7 @@ import Dashboard from "./scenes/dashboard";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 
 function App() {
@@ -22,8 +24,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
