adb8c60d440e13f5382bf18ec70bd6200ec80838
Dhruv Mohindru <dmohindru@gmail.com>
2023-11-21 20:18:25 +1100

    making progress

--- mui-react-responsive/react-mui-responsive-frontend/src/App.tsx
+++ mui-react-responsive/react-mui-responsive-frontend/src/App.tsx
@@ -1,28 +1,16 @@
-import { Button, Typography } from "@mui/material";
-
-import {Add, Settings } from '@mui/icons-material';
-
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  
+
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained" color="secondary" size="small">Settings</Button>
-      <Button startIcon={<Add />} variant="contained" color="success" size="small">Add new posts</Button>
-      <Button variant="outlined" disabled>Outlined</Button>
-      <Typography variant="h1" component="p">It uses h1 style but it's a p tag</Typography>
-      <Button variant="contained" disabled sx={{
-        background: "skyblue",
-        color: "#888",
-        margin: 5,
-        "&:hover": {
-          backgroundColor:"lightblue"
-        },
-        "&:disabled": {
-          backgroundColor: "gray",
-          color: "white"
-        }
-      }}>My Unique Button</Button>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+      
     </div>
   );
 }
--- mui-react-responsive/react-mui-responsive-frontend/src/index.tsx
+++ mui-react-responsive/react-mui-responsive-frontend/src/index.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +8,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
