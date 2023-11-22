17f8bbd0dbc72ebc6bafcce937bc361620c149df
Dhruv Mohindru <dmohindru@gmail.com>
2023-11-22 19:54:53 +1100

    Making progress through right bar

--- mui-react-responsive/react-mui-responsive-frontend/src/components/Navbar.tsx
+++ mui-react-responsive/react-mui-responsive-frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Pets } from "@mui/icons-material";
-import { AppBar, Toolbar, styled, Typography, Box, InputBase } from "@mui/material";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Avatar, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar) ({
     display: "flex",
@@ -14,19 +15,69 @@ const Search = styled("div")(({theme}) => ({
 }))
 
 const Icons = styled(Box)(({theme}) => ({
-    backgroundColor: "white"
+    display: "none",
+    gap: "20px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+        display: "flex"
+    }
+    
 }))
+
+const UserBox = styled(Box)(({theme}) => ({
+    display: "flex",
+    gap: "10px",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+        display: "none"
+    }
+})) 
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <AppBar position="sticky">
             <StyledToolbar>
                 <Typography variant="h6" sx={{display:{xs:"none", sm:"block"}}}>dmohindru.dev</Typography>
                 <Pets sx={{display:{xs:"block", sm:"none"}}}/>
                 <Search><InputBase placeholder="search..." /></Search>
-                <Icons>Icons</Icons>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={2} color="error">
+                        <Notifications />
+                    </Badge>
+                    <Avatar 
+                        sx={{width: 30, height: 30}} 
+                        src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                        onClick={(e)=> setOpen(true)}
+                    />
+                </Icons>
+                <UserBox onClick={(e)=> setOpen(true)}>
+                    <Avatar sx={{width: 30, height: 30}} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+                    <Typography variant="h6">John</Typography>
+                </UserBox>
             </StyledToolbar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={(e)=> setOpen(false)}
+                anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>My account</MenuItem>
+            <MenuItem>Logout</MenuItem>
+        </Menu>
         </AppBar>
     )
 }
--- mui-react-responsive/react-mui-responsive-frontend/src/components/Rightbar.tsx
+++ mui-react-responsive/react-mui-responsive-frontend/src/components/Rightbar.tsx
@@ -1,16 +1,47 @@
-import { Box } from "@mui/material";
+import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
 
 const Rightbar = () => {
     return (
-        <Box 
-            bgcolor="lightcoral" 
-            flex={2} 
-            p={2}
-            sx={{
-                display: {xs: "none", sm: "block"}
-            }}
-        
-        >Rightbar</Box>
+        <Box flex={2} p={2} sx={{ display: {xs: "none", sm: "block"}}}>
+            <Box position="fixed">
+                <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
+                    <AvatarGroup max={7}>
+                    <Avatar
+                        alt="Remy Sharp"
+                        src="https://material-ui.com/static/images/avatar/1.jpg"
+                    />
+                    <Avatar
+                        alt="Travis Howard"
+                        src="https://material-ui.com/static/images/avatar/2.jpg"
+                    />
+                    <Avatar
+                        alt="Cindy Baker"
+                        src="https://material-ui.com/static/images/avatar/3.jpg"
+                    />
+                    <Avatar alt="Agnes Walker" src="" />
+                    <Avatar
+                        alt="Trevor Henderson"
+                        src="https://material-ui.com/static/images/avatar/6.jpg"
+                    />
+                    <Avatar
+                        alt="Trevor Henderson"
+                        src="https://material-ui.com/static/images/avatar/7.jpg"
+                    />
+                    <Avatar
+                        alt="Trevor Henderson"
+                        src="https://material-ui.com/static/images/avatar/8.jpg"
+                    />
+                    <Avatar
+                        alt="Trevor Henderson"
+                        src="https://material-ui.com/static/images/avatar/7.jpg"
+                    />
+                    <Avatar
+                        alt="Trevor Henderson"
+                        src="https://material-ui.com/static/images/avatar/8.jpg"
+                    />
+                </AvatarGroup>
+            </Box>
+        </Box>
     )
 }
 
