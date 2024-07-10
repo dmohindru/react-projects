5abed8fab17e9e32b8d718b7d31e21dd8d43c5fe
Dhruv Mohindru <dmohindru@gmail.com>
2024-07-10 18:52:30 +1000

    Started creating Weather Component

---
+++ react-custom-hooks-demo/custom-hooks-demo1/src/components/weather/Weather.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentContainer from '../ComponentContainer';
+
+const Weather: React.FC = () => {
+  return (
+    <ComponentContainer>
+      <div>Weather Component</div>
+    </ComponentContainer>
+  );
+};
+
+export default Weather;
