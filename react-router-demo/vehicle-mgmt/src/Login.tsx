ad2540f77d04ca3f7ebc6c11e4aeee165396e438
Dhruv Mohindru <dmohindru@gmail.com>
2024-08-20 09:04:12 +1000

    Working on Login Component

--- react-router-demo/vehicle-mgmt/src/Login.tsx
+++ react-router-demo/vehicle-mgmt/src/Login.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import React, { ChangeEvent } from 'react';
+import {
+  Box,
+  Typography,
+  Container,
+  Paper,
+  TextField,
+  Button,
+} from '@mui/material';
+import { useState, useEffect } from 'react';
 
 export const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(true);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  useEffect(() => {
+    setDisabled(username.trim() === '');
+  }, [username]);
+
   return (
     <Container maxWidth="md">
       <Box
@@ -11,8 +29,36 @@ export const Login: React.FC = () => {
         alignContent="center"
         alignItems="center"
       >
-        <Typography variant="h4">Vehicle Management Software</Typography>
-        <Typography variant="h5">Login</Typography>
+        <Typography variant="h4" sx={{ mt: 15 }}>
+          Fleet Master
+        </Typography>
+        <Paper
+          elevation={3}
+          square={false}
+          sx={{ width: '50%', mt: 7, py: 4, px: 4 }}
+        >
+          <Box display="flex" flexDirection="column">
+            <Typography
+              variant="h5"
+              display="flex"
+              justifyContent="center"
+              my={3}
+            >
+              Login
+            </Typography>
+            <TextField
+              label="Username"
+              variant="outlined"
+              size="small"
+              sx={{ my: 3 }}
+              onChange={handleInputChange}
+              value={username}
+            />
+            <Button variant="contained" disabled={disabled} sx={{ my: 3 }}>
+              Submit
+            </Button>
+          </Box>
+        </Paper>
         <Box></Box>
       </Box>
     </Container>
