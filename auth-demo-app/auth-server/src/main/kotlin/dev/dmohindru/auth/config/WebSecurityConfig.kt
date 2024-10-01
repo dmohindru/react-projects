5e984f14f350adf768338a8a157a5ecc18bb4400
Dhruv Mohindru <dmohindru@gmail.com>
2024-10-02 09:02:28 +1000

    Created auth server with basic config

---
+++ auth-demo-app/auth-server/src/main/kotlin/dev/dmohindru/auth/config/WebSecurityConfig.kt
@@ -0,0 +1,175 @@
+package dev.dmohindru.auth.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig {
+
+    @Bean
+    @Order(1)
+    @Throws(Exception::class)
+    fun authorizationServerSecurityFilter(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer::class.java)
+            .oidc(Customizer.withDefaults())
+
+        http
+            .cors(Customizer.withDefaults())
+            .exceptionHandling {
+                it.defaultAuthenticationEntryPointFor(
+                    LoginUrlAuthenticationEntryPoint("http://localhost:3000/login"),
+                    MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+            }
+            .oauth2ResourceServer {
+                it.jwt(Customizer.withDefaults())
+            }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    @Throws(Exception::class)
+    fun authenticationSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .securityMatcher("/login", "/logout", "/register")
+            .cors(Customizer.withDefaults())
+            .csrf { it.disable() }
+            .formLogin {
+                it
+                    .loginPage("http://localhost:3000/login")
+                    .loginProcessingUrl("/login")
+                    .successHandler { _, res, _ ->
+                        res.resetBuffer()
+                        res.status = HttpStatus.OK.value()
+                        res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        res.writer.append("login successful success")
+                        res.flushBuffer()
+                    }
+                    .failureHandler { _, res, _ -> res.status = HttpStatus.UNAUTHORIZED.value() }
+            }
+            .logout {
+                it.logoutSuccessUrl("http://localhost:3000/login?logout")
+            }
+            .exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.FORBIDDEN))
+            }
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/login", "/register").permitAll()
+                    .anyRequest().authenticated()
+            }
+
+        return http.build()
+    }
+
+    @Bean
+    @Order(3)
+    @Throws(Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        config.addAllowedOrigin("http://localhost:3000")
+        config.allowCredentials = true
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
+
+    @Bean
+    fun registerClientRepository(): RegisteredClientRepository {
+        val publicClient = RegisteredClient.withId(UUID.randomUUID().toString())
+            .clientId("frontend-react-app")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+            .redirectUri("http://localhost:3000/callback")
+            .scope(OidcScopes.OPENID)
+            .scope(OidcScopes.PROFILE)
+            .clientSettings(ClientSettings.builder()
+                .requireAuthorizationConsent(false)
+                .requireProofKey(true)
+                .build()
+            )
+            .build()
+
+        val backendClient = RegisteredClient.withId(UUID.randomUUID().toString())
+            .clientId("backend-client")
+            .clientSecret("{noop}password")
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+            .scope("read")
+            .scope("write")
+            .clientSettings(ClientSettings.builder()
+                .requireAuthorizationConsent(false)
+                .build())
+            .build()
+
+        return InMemoryRegisteredClientRepository(publicClient, backendClient)
+    }
+
+    @Bean
+    fun userDetailService(passwordEncoder: PasswordEncoder): UserDetailsService {
+        val user1 = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("password"))
+            .build()
+
+        val user2 = User.builder()
+            .username("user")
+            .password("password")
+            .build()
+
+        return InMemoryUserDetailsManager(user1, user2)
+    }
+
+    @Bean
+    fun passEncoder(): PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+
+
+}
