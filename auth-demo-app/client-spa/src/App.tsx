8ecde479ee7c2566f1ead24a60b530f46e45793d
Dhruv Mohindru <dmohindru@gmail.com>
2024-10-07 13:31:50 +1100

    Created Welcome page

--- auth-demo-app/client-spa/src/App.tsx
+++ auth-demo-app/client-spa/src/App.tsx
@@ -1,6 +1,59 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import {
+  Container,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+  styled,
+} from '@mui/material';
+
+const CenteredTypography = styled(Typography)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  display: 'flex',
+  justifyContent: 'center',
+  fontWeight: 'bold',
+}));
 
 export const App: React.FC = () => {
-  return <Outlet />;
+  return (
+    <Container maxWidth="md">
+      <Box
+        margin={3}
+        display="flex"
+        alignContent="center"
+        justifyContent="center"
+      >
+        <Card sx={{ mt: 4, maxWidth: '600' }}>
+          <CardMedia
+            sx={{ height: 500, width: 500 }}
+            image="/app-logo.jpg"
+            title="app-logo"
+          />
+          <CardContent sx={{ border: 1 }}>
+            <Box display="flex" flexDirection="column">
+              <CenteredTypography variant="h4">
+                Welcome to WiseCrack!
+              </CenteredTypography>
+              <CenteredTypography variant="body1">
+                Your daily does of humor and wisdom
+              </CenteredTypography>
+              <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-around"
+              >
+                <Button>Login</Button>
+                <Button disabled>Register</Button>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </Container>
+  );
 };
