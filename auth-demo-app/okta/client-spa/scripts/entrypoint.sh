54d40187cf96639bed4ca208923b7af38ecc3110
Dhruv Mohindru <dmohindru@gmail.com>
2024-12-03 20:26:32 +1100

    Create Dockerfile to run react app in different run profiles

--- auth-demo-app/okta/client-spa/scripts/entrypoint.sh
+++ auth-demo-app/okta/client-spa/scripts/entrypoint.sh
@@ -6,15 +6,18 @@ case "$RUN_PROFILE" in
     CONFIG_JSON_PATH="/etc/react-app/config-cluster1.json"
     ;;
   cluster2)
-    CONFIG_JSON_PATH="/etc/react-app/config-cluster.json"
+    CONFIG_JSON_PATH="/etc/react-app/config-cluster2.json"
     ;;
   *)
     CONFIG_JSON_PATH="/etc/react-app/config-default.json"
     ;;
 esac
 
+echo "Run Profile: $RUN_PROFILE"
+echo "Config file path: $CONFIG_JSON_PATH"
+
 # Replace placeholder in Apache config template
-sed "s|{{CONFIG_JSON_PATH}}|$CONFIG_JSON_PATH|g" /usr/local/apache2/conf/httpd.conf.template > /usr/local/apache2/conf/httpd.conf
+ln -sf "$CONFIG_JSON_PATH" /usr/local/apache2/htdocs/config.json
 
 # Start Apache
 httpd-foreground
